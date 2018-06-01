e94b2ad88f67885ae572e978a45be256235ffbb9
Mathias Daniel Battistella Suarez <mathias.battistella@fing.edu.uy>
2018-06-01 01:46:34 -0300

    Up

---
+++ IIterator.h
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   IIterator.h
+ * Author: user6
+ *
+ * Created on 1 de junio de 2018, 0:56
+ */
+
+#ifndef IITERATOR_H
+#define IITERATOR_H
+
+class IIterator {
+public:
+    IIterator();
+    IIterator(const IIterator& orig);
+    virtual ~IIterator();
+private:
+
+};
+
+#endif /* IITERATOR_H */
+
